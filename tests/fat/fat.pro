77989178977ce8aeebc68c30eb66d901f9fb92d6
Richard Braakman <richard.braakman@jollamobile.com>
2014-05-09 21:31:26 +0300

    [tests] Add test for entries past end of fat

--- tests/fat/fat.pro
+++ tests/fat/fat.pro
@@ -1,10 +1,12 @@
 QT += testlib
 QT -= gui
+CONFIG += debug
 TARGET = test-fat
 DEPENDPATH += ../..
 INCLUDEPATH += ../..
 QMAKE_LFLAGS += -fprofile-arcs -ftest-coverage
-QMAKE_CXXFLAGS += -fprofile-arcs -ftest-coverage -g -O0
+QMAKE_CXXFLAGS += -fprofile-arcs -ftest-coverage
+QMAKE_CXXFLAGS_DEBUG += -O0
 
 # Input
 SOURCES += tst_fat.cpp
--- tests/fat/tst_fat.cpp
+++ tests/fat/tst_fat.cpp
@@ -33,10 +33,19 @@ int dir_fill(char *buf, uint32_t len, int, uint32_t)
     return 0;
 }
 
+// Helper macro. Has to be a macro because QCOMPARE can only be
+// used from the test function itself.
+#define call_fat_fill(buf, entry_nr, entries) \
+    uint32_t buf[(entries) + 1]; \
+    buf[entries] = 0x31337; \
+    fat_fill(buf, entry_nr, entries); \
+    QCOMPARE(buf[entries], (uint32_t) 0x31337);
+
 class TestFat : public QObject {
     Q_OBJECT
 
-    static const int DATA_CLUSTERS = 1000000;
+    static const uint32_t DATA_CLUSTERS = 1000000;
+    static const uint32_t FAT_ENTRIES = DATA_CLUSTERS + 2;
 
 private slots:
     void init() {
@@ -48,31 +57,45 @@ private slots:
         QCOMPARE(fat_dir_index(0), -1);
         QCOMPARE(fat_dir_index(1000), -1);
 
-	fat_finalize(DATA_CLUSTERS); // required by API
+        fat_finalize(DATA_CLUSTERS); // required by API
 
-	uint32_t buf[1024 + 1];
-	buf[1024] = 0x31337; // buffer overflow guard value
-	fat_fill(buf, 0, 1024);
-	QCOMPARE(buf[1024], (uint32_t) 0x31337);
-	// Check the special first two fat entries
-	QCOMPARE(buf[0], (uint32_t) 0x0ffffff8); // media byte marker
-	QCOMPARE(buf[1], (uint32_t) 0x0fffffff); // end of chain marker
-	// everything else should be 0
-	for (int i = 2; i < 1024; i++) {
-		QCOMPARE(buf[i], (uint32_t) 0);
-	}
+        call_fat_fill(buf, 0, 1024);
+        // Check the special first two fat entries
+        QCOMPARE(buf[0], (uint32_t) 0x0ffffff8); // media byte marker
+        QCOMPARE(buf[1], (uint32_t) 0x0fffffff); // end of chain marker
+        // everything else should be 0
+        for (int i = 2; i < 1024; i++) {
+        	QCOMPARE(buf[i], (uint32_t) 0);
+        }
 
-	char data[4096 + 1];
-	data[4096] = 'X'; // buffer overflow guard
-	uint32_t filled;
-	int ret = data_fill(data, 4096, 0, 0, &filled);
-	QCOMPARE(data[4096], 'X');
-	QCOMPARE(ret, 0);
-	QCOMPARE(filled, (uint32_t) 4096);
-	// everything should be 0
-	for (int i = 0; i < 4096; i++) {
-		QCOMPARE(data[i], (char) 0);
-	}
+        char data[4096 + 1];
+        data[4096] = 'X'; // buffer overflow guard
+        uint32_t filled;
+        int ret = data_fill(data, 4096, 0, 0, &filled);
+        QCOMPARE(data[4096], 'X');
+        QCOMPARE(ret, 0);
+        QCOMPARE(filled, (uint32_t) 4096);
+        // everything should be 0
+        for (int i = 0; i < 4096; i++) {
+        	QCOMPARE(data[i], (char) 0);
+        }
+    }
+
+    void test_end_of_fat() {
+        uint32_t last_page_start = FAT_ENTRIES - (FAT_ENTRIES % 1024);
+
+        fat_finalize(DATA_CLUSTERS);
+
+        call_fat_fill(buf, last_page_start, 1024);
+	// All valid fat entries should still be 0, and the rest should
+	// contain bad cluster markers.
+        for (uint32_t i = last_page_start; i < FAT_ENTRIES; i++) {
+            QCOMPARE(buf[i - last_page_start], (uint32_t) 0);
+        }
+        for (uint32_t i = FAT_ENTRIES; i < last_page_start + 1024; i++) {
+            // Bad cluster marker
+            QCOMPARE(buf[i - last_page_start], (uint32_t) 0x0ffffff7);
+        }
     }
 
 };
