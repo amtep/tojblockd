f6a51ab0d3ae8977206ba7eee24580e2bc6f7b7e
Richard Braakman <richard.braakman@jollamobile.com>
2014-05-13 17:38:48 +0300

    [licenses] Update copyright years

--- vfat.cpp
+++ vfat.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Jolla Ltd.
+ * Copyright (C) 2013-2014 Jolla Ltd.
  * Contact: Richard Braakman <richard.braakman@jollamobile.com>
  *
  * This program is free software; you can redistribute it and/or
